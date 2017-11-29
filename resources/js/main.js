c6a239500fd13df183b2b19e2df1fcb7092514d7
Chip-L <chip_long@yahoo.com>
2017-11-29 00:10:47 -0700

    List Stories - can't select

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,6 @@
 /**************** Mad Libs main ****************/
 
-function getJSON() {
+var getJSON = function() {
   var textJSON = '{ "MadLibsStories":[ ' +
                  '  { ' +
                  '	"StoryTitle":"Story 1", ' +
@@ -12,36 +12,45 @@ function getJSON() {
   return JSON.parse(textJSON);
 };
 
-function putJSON() {
+var putJSON = function() {
 
 };
 
-function selectStory(list) {
+var selectStory = function(list) {
   var i=0;
   var dispText = '';
+
+  $('#storybody').empty(); // this will delete what is in the storybody div already
+  $('#storybody').append("<h2>Story List</h2>");
   if(list.MadLibsStories.length <= 0) {
     dispText = '' +
       '<h2>Warning!</h2>' +
       '<p>You do not have any stories created. Please create a story</p>';
   } else {
-    dispText = '<h2>Story List</h2>';
-    while (i <= list.MadLibsStories.length) {
-      dispText += dispText + '<button class="story_list_item" onclick="return ' + i +'">' + list.MadLibsStories[i].StoryTitle + '</button>';
+    //dispText = '<h2>Story List</h2>';
+    while (i < list.MadLibsStories.length) {
+      console.log (list.MadLibsStories[i].StoryTitle);
+      dispText += '<button class="story_list_item" onclick="function(return ' + i + ')">' + list.MadLibsStories[i].StoryTitle + '</button>';
+      i++;
     }
   };
-  document.getElementById("storybody").innerHTML= dispText;
+  $("#storybody").append(dispText);
 
 };
 
-function getWords() {
+var getWords = function() {
 
 };
 
-function makeStory() {
+var makeStory = function() {
 
 };
 
-function readStory() {
+var readStory = function() {
   storyList = getJSON();
   storyId = selectStory(storyList)
+}
+
+var main = function() {
+
 }
